55b1d016cdc2471ea74971b505f3c2682c6ea836
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2023-06-01 16:41:52 +0900

    style: fix style

--- src/layouts/Profile.tsx
+++ src/layouts/Profile.tsx
@@ -3,8 +3,8 @@ import ProfileText from "@components/ProfileText";
 
 const Profile: React.FC = () => {
   return (
-    <div className="flex flex-col w-full h-full sm:w-full md:w-full xs:w-full xs:-mt-5 xxs:w-fit xxs:-mt-5">
-      <div className="m-2 pb-2 w-auto sm:w-auto md:w-auto xs:w-auto xxs:w-[101%]">
+    <div className="flex flex-col w-full h-full sm:w-fit md:w-fit xs:w-full xs:-mt-5 xxs:w-fit xxs:-mt-5">
+      <div className="m-2 pb-2 w-auto sm:w-auto md:w-auto xs:w-auto xxs:w-[107%]">
         <p
           id="profile"
           className="text-2xl font-bold pl-2 bg-blue-900 p-[0.5px] mt-1 sm:text-sm md:text-base xs:text-sm xxs:text-sm"
@@ -12,7 +12,7 @@ const Profile: React.FC = () => {
           Profile.
         </p>
       </div>
-      <div className="flex flex-row justify-start gap-20 box-border w-auto h-full m-5 sm:w-auto sm:gap-10 md:w-auto md:gap-10 xs:flex-col xs:w-full xs:gap-10 xs:items-center xs:-ml-5 xxs:flex-col xxs:w-full xxs:gap-10 xxs:items-center">
+      <div className="flex flex-row justify-start gap-20 box-border w-auto h-full m-5 sm:w-auto sm:gap-10 md:w-auto md:gap-10 xs:flex-col xs:w-full xs:gap-10 xs:items-center xs:-ml-5 xxs:flex-col xxs:w-full xxs:ml-8 xxs:gap-10 xxs:items-center">
         <ProfileText />
         <IntroduceComponents />
       </div>
